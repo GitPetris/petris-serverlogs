c10086b158084b684e354669c223ab08c4272fa6
Petris <62209057+GitPetris@users.noreply.github.com>
2020-09-24 19:10:20 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
-Config.JoinedWebhookURL = "https://discordapp.com/api/webhooks/758620848332079145/37uXqJVSK6B_WgRPorguuXr7v8KARPYibmowA48Y_gxZyWS86QWvyH9L3-S25ZrmrcPI"
-Config.LeftWebhookURL = "https://discordapp.com/api/webhooks/758620981999960114/RIdAjISsNslfuILNP5oCdPESEauGnJdj7_u2q0c-dyFoh0l-JWZ0-Hd-Dt3FPmsJJ0gj"
+Config.JoinedWebhookURL = ""
+Config.LeftWebhookURL = ""
